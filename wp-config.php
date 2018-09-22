a4ce53267e655eee72575626a04a4a4f3ebb88e6
lbbniu <lbbniu@qq.com>
2018-09-22 14:31:50 +0800

    删除疑似安全隐患

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@ define('WP_SITEURL', 'https://' . $_SERVER['HTTP_HOST']);
 define('WP_HOME', 'https://' . $_SERVER['HTTP_HOST']);
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'wp');
+define('DB_NAME', 'blog');
 
 /** MySQL数据库用户名 */
-define('DB_USER', 'root');
+define('DB_USER', getenv('DB_USER'));
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', 'lbbniu123456');
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
 
 /** MySQL主机 */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', getenv('DB_HOST'));
 
 /** 创建数据表时默认的文字编码 */
 define('DB_CHARSET', 'utf8');
