b4ce1e69230329584ab4029658f85451fcbf0dbc
lbbniu <lbbniu@qq.com>
2018-09-22 18:05:25 +0800

    更新wordpress

--- wp-content/themes/lbbniu1.0/footer.php
+++ wp-content/themes/lbbniu1.0/footer.php
@@ -2,7 +2,7 @@
 <footer class="footer">
     <div class="footer-inner">
         <div class="copyright pull-left">
-         <a href="http://www.lbbniu.com/" title="无限飞翔">无限飞翔</a> 版权所有，保留一切权利 · <a href="http://www.lbbniu.com/sitemap.xml" title="站点地图">站点地图</a>   ·   基于WordPress构建   © 2011-2014<!--  ·   托管于 <a rel="nofollow" target="_blank" href="http://yusi123.com/go/aliyun">阿里云主机</a> & <a rel="nofollow" target="_blank" href="http://yusi123.com/go/qiniu">七牛云存储</a>-->
+         <a href="https://www.lbbniu.com/" title="无限飞翔">无限飞翔</a> 版权所有，保留一切权利 · <a href="https://www.lbbniu.com/sitemap.xml" title="站点地图">站点地图</a>   ·   基于WordPress构建   © 2011-2014<!--  ·   托管于 <a rel="nofollow" target="_blank" href="http://yusi123.com/go/aliyun">阿里云主机</a> & <a rel="nofollow" target="_blank" href="http://yusi123.com/go/qiniu">七牛云存储</a>-->
 		 <a rel="nofollow" target="_blank" href="http://www.miitbeian.gov.cn/">京ICP备14053647号-1</a>
 		 
         </div>
