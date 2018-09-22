0212f11ac3e051cd9c01018f5fafc13899785682
lbbniu <lbbniu@qq.com>
2018-09-22 20:10:52 +0800

    更新wordpress主题png js

--- wp-content/themes/lbbniu1.0/footer.php
+++ wp-content/themes/lbbniu1.0/footer.php
@@ -16,7 +16,7 @@
 wp_footer(); 
 global $dHasShare; 
 if($dHasShare == true){ 
-	echo'<script>with(document)0[(getElementsByTagName("head")[0]||body).appendChild(createElement("script")).src="http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion="+~(-new Date()/36e5)];</script>';
+	echo'<script>with(document)0[(getElementsByTagName("head")[0]||body).appendChild(createElement("script")).src="/static/api/js/share.js?v=89860593.js?cdnversion="+~(-new Date()/36e5)];</script>';
 }  
 if( dopt('d_footcode_b') ) echo dopt('d_footcode'); 
 ?>
