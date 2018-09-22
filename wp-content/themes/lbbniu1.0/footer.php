cfe0edc58db15a88f51cc4d839fe0e3b223ca3f5
lbbniu <lbbniu@qq.com>
2018-09-22 20:24:46 +0800

    更新wordpress主题png js

--- wp-content/themes/lbbniu1.0/footer.php
+++ wp-content/themes/lbbniu1.0/footer.php
@@ -15,7 +15,6 @@
 <?php 
 wp_footer(); 
 global $dHasShare;
-var_dump($dHasShare);
 if($dHasShare == true){ 
 	echo'<script>with(document)0[(getElementsByTagName("head")[0]||body).appendChild(createElement("script")).src="//www.lbbniu.com/static/api/js/share.js?v=89860593.js?cdnversion="+~(-new Date()/36e5)];</script>';
 }  
